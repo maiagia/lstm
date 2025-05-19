eb5b12cbac00517f68c110ebf8549b57874265b8
klerytondev <kleryton.dev@gmail.com>
2025-05-19 20:34:02 -0300

    Implement API for historical stock price retrieval and future price prediction
    - Created Dockerfile for API setup.
    - Added FastAPI application structure with main entry point.
    - Developed models for request and response handling using Pydantic.
    - Implemented routes for obtaining historical prices and predicting future prices.
    - Integrated yfinance for fetching historical stock data.
    - Set up service functions for handling business logic related to stock data.
    - Developed a Streamlit frontend for user interaction and data visualization.
    - Added requirements for necessary packages in requirements-api.txt.

--- request_historico_preco/app.py
+++ request_historico_preco/app.py
@@ -2,6 +2,7 @@ import requests
 import pandas as pd
 import streamlit as st
 from datetime import datetime, timedelta
+
 # Configuração da interface com Streamlit
 st.title("TechChallenge - Fase 04")
 st.write("Este aplicativo permite consultar o histórico de preços de ações da B3.")
@@ -11,11 +12,12 @@ st.write("**Membros do projeto:** Kleryton de Souza, Lucas Paim, Maiara Giavoni,
 acoes = st.multiselect(
     "Selecione as ações:",
     options=[
-        'PETR4.SA', 
+        'PETR4.SA', 'DIS',
         # 'VALE3.SA', 'ITUB4.SA', 'BBAS3.SA', 'BBDC4.SA', 
         # 'ABEV3.SA', 'MGLU3.SA', 'WEGE3.SA', 'RENT3.SA', 'B3SA3.SA'
     ],
-    default=['PETR4.SA']
+    default=['DIS']
+    # default=['PETR4.SA']
 )
 
 hoje = datetime.today()
@@ -24,9 +26,8 @@ um_ano_atras = hoje - timedelta(days=365)
 data_inicio = st.date_input("Data de início:", value=um_ano_atras)
 data_fim = st.date_input("Data de fim:", value=hoje)
 
-if st.button("Consultar"):
+def consultar_historico(acoes, data_inicio, data_fim):
     vEndPoint = 'http://localhost:8000/api/historico_preco'
-    # vEndPoint = 'http://api:8000/api/historico_preco'
     vBase = pd.DataFrame()
 
     for acao in acoes:
@@ -35,9 +36,7 @@ if st.button("Consultar"):
             'data_inicio': data_inicio.strftime('%Y-%m-%d'),
             'data_fim': data_fim.strftime('%Y-%m-%d')
         }
-
         vResponse = requests.post(vEndPoint, json=vPayload)
-
         if vResponse.status_code == 200:
             vBase_Json = vResponse.json()
             vBaseTemp = pd.DataFrame(vBase_Json)
@@ -46,18 +45,20 @@ if st.button("Consultar"):
             st.error(f"Erro ao consultar a ação {acao}: {vResponse.status_code}")
 
     if not vBase.empty:
-        # Tratando valores nulos
         vBase.fillna(0, inplace=True)
-
-        # Convertendo a coluna 'Date' para o formato datetime
         vBase['Date'] = pd.to_datetime(vBase['Date'])
+    return vBase, data_inicio, data_fim
 
-        # Exibindo os dados na interface
+if st.button("Consultar"):
+    vBase, data_inicio, data_fim = consultar_historico(acoes, data_inicio, data_fim)
+
+    if not vBase.empty:
         st.dataframe(vBase)
-
-        # Exporta para CSV
         csv_filename = f"saida_historico_csv/historico_precos_{datetime.now().strftime('%Y%m%d%H%M%S')}.csv"
         vBase.to_csv(csv_filename, index=False)
         st.success(f"Arquivo CSV exportado: {csv_filename}")
+        # Exemplo de uso das datas:
+        st.write(f"Data de início usada: {data_inicio}")
+        st.write(f"Data de fim usada: {data_fim}")
     else:
         st.warning("Nenhum dado retornado para os parâmetros informados.")
